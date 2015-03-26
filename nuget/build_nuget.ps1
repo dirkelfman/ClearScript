a5b04dafe069d25ceb96e142d836a4f6d1646d21
Sparkel Phipps <thomas_phipps@volusion.com>
2015-03-26 08:56:04 -0500

    adding sha to nupack

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -1,8 +1,12 @@
 
-Remove-Item .\*.nupkg
+Remove-Item *.nupkg
+$sha = git rev-parse HEAD
 $path=(get-item ".\..\bin\Release\ClearScript.dll").FullName
 $ass = [Reflection.Assembly]::Loadfile($path)
 $ver = $ass.GetName().version.ToString()
 (Get-Content ClearScript.Installer.targets) | Foreach-Object {$_ -replace "ClearScript.Installer.[^\\]*", "ClearScript.Installer.$ver"} | Set-Content ClearScript.Installer.targets
-nuget pack ClearScript.Installer.nuspec -Properties version=$ver;
+
+
+
+nuget pack ClearScript.Installer.nuspec -Properties version=$ver`;sha=$sha
 (Get-Content ClearScript.Installer.targets) | Foreach-Object {$_ -replace "ClearScript.Installer.[^\\]*", "ClearScript.Installer.version"} | Set-Content ClearScript.Installer.targets
