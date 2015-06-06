04e51d56de23d006909e202255087ba53630c8f6
Sparkel Phipps <thomas_phipps@volusion.com>
2015-06-06 07:15:55 -0500

    modifying nuget target to work with paket

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -13,9 +13,9 @@ $sha = git rev-parse HEAD
 $path=(get-item ".\..\bin\Release\ClearScript.dll").FullName
 $ass = [Reflection.Assembly]::Loadfile($path)
 $ver = $ass.GetName().version.ToString()
-(Get-Content ClearScript.Installer.targets) | Foreach-Object {$_ -replace "ClearScript.Installer.[^\\]*", "ClearScript.Installer.$ver"} | Set-Content ClearScript.Installer.targets
+(Get-Content ClearScript.Installer.targets.template) | Foreach-Object {$_ -replace "TOKEN", $ver} | Set-Content ClearScript.Installer.targets
 
 
 
 nuget pack ClearScript.Installer.nuspec -Properties version=$ver`;sha=$sha
-(Get-Content ClearScript.Installer.targets) | Foreach-Object {$_ -replace "ClearScript.Installer.[^\\]*", "ClearScript.Installer.version"} | Set-Content ClearScript.Installer.targets
+Installer.targets
